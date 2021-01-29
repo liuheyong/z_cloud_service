991a4036ae28ea56f81b1e4e1f00f3d67863e80c
liuheyong <17682347237@163.com>
2021-01-29 23:25:54 +0800

    2021年01月24日19:48:50

--- src/main/java/com/cloud/service/impl/ECooperateMerServiceImpl.java
+++ src/main/java/com/cloud/service/impl/ECooperateMerServiceImpl.java
@@ -8,6 +8,8 @@ import com.cloud.service.SpringUtil;
 import com.cloud.service.aop.AopTest;
 import com.cloud.service.mapper.ECooperateMerMapper;
 import com.cloud.service.utils.UUIDUtil;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -82,23 +84,23 @@ public class ECooperateMerServiceImpl implements ECooperateMerService {
         try {
             // 批量插入数据测试
             List<ECooperateMer> ecList = new ArrayList<>();
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 50000; i++) {
                 ECooperateMer mer = new ECooperateMer(UUIDUtil.getUNIDX("EC", 30), "A2019022200000001", "测试数据添加", "1556442573307.jpg", "https://www.baidu.com", "1", 12);
                 ecList.add(mer);
             }
-            //SqlSession sqlSession = sqlSessionTemplate.getSqlSessionFactory().openSession(ExecutorType.BATCH, false);
-            //ECooperateMerMapper mapper = sqlSession.getMapper(ECooperateMerMapper.class);
+            SqlSession sqlSession = sqlSessionTemplate.getSqlSessionFactory().openSession(ExecutorType.BATCH, false);
+            ECooperateMerMapper mapper = sqlSession.getMapper(ECooperateMerMapper.class);
             long startTime = System.currentTimeMillis();
             ecList.stream().forEach(item -> {
-                //try {
-                eCooperateMerMapper.addECooperateMerInfo(item);
+                try {
+                    mapper.addECooperateMerInfo(item);
                 //int a = 10 / 0;
-                //} catch (Exception e) {
-                //    sqlSession.rollback();
-                //    logger.error(e.getMessage(), e);
-                //}
+                } catch (Exception e) {
+                    sqlSession.rollback();
+                    logger.error(e.getMessage(), e);
+                }
             });
-            //sqlSession.commit();
+            sqlSession.commit();
             long endTime = System.currentTimeMillis();
             System.out.println("==============批量插入"+ ecList.size() +"条数据测试耗时==============" + (endTime - startTime));
             //logger.info(String.valueOf(RpcContext.getContext().getAttachment("myKey")));
